fc5e169bc2bfed5bb0498ca49af837286741ff86
reneeb <info@perl-services.de>
2022-05-04 09:22:45 +0200

    fix copying in modern browsers

--- Kernel/Output/HTML/FilterElementPost/CopyTicketNumberToClipboard.pm
+++ Kernel/Output/HTML/FilterElementPost/CopyTicketNumberToClipboard.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2017 - 2018 Perl-Services.de, http://www.perl-services.de/
+# Copyright (C) 2017 - 2022 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -66,6 +66,8 @@ sub _BuildHTML {
     my $Divider = $ConfigObject->Get('Ticket::HookDivider');
 
     my $TicketNumber = $Param{Tn};
+    $TicketNumber =~ s{\A\s*\[}{};
+    $TicketNumber =~ s{\]\s*\z}{};
     $TicketNumber =~ s{\A$Hook$Divider}{};
 
     my $CopyValue = $LayoutObject->Output(
@@ -81,41 +83,57 @@ sub _BuildHTML {
         <script type="text/javascript">//<![CDATA[
              function copyToClipboard(text) {
                 $('#CopyTicketNumberIcon').addClass('pulsing');
-                if (window.clipboardData) { // Internet Explorer
-                    window.clipboardData.setData("Text", text);
-                } else {  
-                    var target = document.createElement("textarea");
-                    target.style.position = "absolute";
-                    target.style.left = "10px";
-                    target.style.top = "0";
-                    target.textContent = text;
 
-                    document.body.appendChild(target);
+                if ( !navigator.clipboard ) {
+                    $('#CopyTicketNumberIcon').addClass('pulsing');
+                    if (window.clipboardData) { // Internet Explorer
+                        window.clipboardData.setData("Text", text);
+                    } else {
+                        var target = document.createElement("textarea");
+                        target.style.position = "absolute";
+                        target.style.left = "10px";
+                        target.style.top = "0";
+                        target.textContent = text;
 
-                    var range = document.createRange();  
-                    range.selectNode(target);  
-                    window.getSelection().addRange(range);  
+                        document.body.appendChild(target);
 
-                    try {
-                      var successful = document.execCommand('copy');
-                      var msg = successful ? 'successful' : 'unsuccessful';
-                      //console.log('Copying text command was ' + msg);
-                    } catch (err) {
-                      //console.log('Oops, unable to copy');
+                        var range = document.createRange();
+                        range.selectNode(target);
+                        window.getSelection().addRange(range);
+
+                        try {
+                          var successful = document.execCommand('copy');
+                          var msg = successful ? 'successful' : 'unsuccessful';
+                          //console.debug('Copying text command was ' + msg);
+                        } catch (err) {
+                          //console.debug('Oops, unable to copy');
+                        }
+
+                        document.body.removeChild(target);
                     }
                 }
+                else {
+                    navigator.clipboard.writeText(text).then( function() {
+                        //console.debug('Copying text command was successful');
+                    }, function (err) {
+                        //console.debug('Oops, unable to copy');
+                    });
+                }
 
-                window.setTimeout( function() { $('#CopyTicketNumberIcon').removeClass('pulsing'); }, 3000 );
+                window.setTimeout( function() { $('#CopyTicketNumberIcon').removeClass('pulsing'); }, 1000 );
             }
         //]]></script>
     ~;
 
     my $HTML = $LayoutObject->Output(
         Template => $HTMLTemplate,
-        Data     => { Value => $CopyValue },
+        Data     => {
+            Value => $CopyValue,
+        },
     );
 
     return $HTML;
 }
 
 1;
+
